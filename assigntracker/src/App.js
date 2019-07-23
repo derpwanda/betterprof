4713b65203d3aa89c9633288297f424cd417adec
Wanda <doubleuevans@gmail.com>
2019-07-22 19:29:08 -0500

    initialize new project, CRA installed and setup

---
+++ assigntracker/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <p>wanda's version of better professor.</p>
+                <p>start</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
