418ae76ddb38f2914ba377c8a25cf56b74e8094e
Wanda <doubleuevans@gmail.com>
2019-07-25 22:00:38 -0500

    updated about page with small blurb

--- assigntracker/src/components/pages/About.js
+++ assigntracker/src/components/pages/About.js
@@ -4,6 +4,11 @@ const About = () => {
     return (
         <Fragment>
             <h1>About the App</h1>
+            <p>
+                Assignment Tracker is an app for teachers to keep track of
+                assignments. Grade submissions, leave notes, remind students of
+                assignments from your dashboard.
+            </p>
         </Fragment>
     );
 };
